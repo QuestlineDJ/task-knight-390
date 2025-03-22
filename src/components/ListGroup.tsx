4b4f58cff5e3c4cd975204873e25d791355f77f9
Quest <mary.kquest@gmail.com>
2025-03-22 18:11:30 -0500

    Add Base Files to Project
    - Attempt to push react and node frameworks to github.

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,25 @@
+//stopped logging at minute 40 in the react video
+
+function ListGroup() {
+  let items = ["New York", "San Fran", "Tokyo", "London", "Paris"];
+
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={() => console.log(item)}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
